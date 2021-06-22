02414d159bd3b873f5341e0b692f830d840ab355
NIR <MAILMAIL10@GMAIL.COM>
2021-06-22 15:56:21 +0300

    daily changes

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike slim short',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '2',
+            name: 'Adidas slim pants',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        
+        {
+            _id: '3',
+            name: 'Lacoste slim short',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 220,
+            brand: 'Lacoste',
+            rating: 4.8,
+            numReviews: 17,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '4',
+            name: 'Nike slim pants',
+            category: 'Pants',
+            image: '/images/p1.jpg',
+            price: 78,
+            brand: 'Nike',
+            rating: 3,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '5',
+            name: 'Puma slim pants',
+            category: 'Pants',
+            image: '/images/p1.jpg',
+            price: 61,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '6',
+            name: 'Adidas slim pants',
+            category: 'Pants',
+            image: '/images/p1.jpg',
+            price: 140,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'high quality product',
+        },
+    ],
+};
+
+export default data;
