3099b75ceb4e6290963f2149cbcc89708c3792c9
NIR <MAILMAIL10@GMAIL.COM>
2021-09-01 21:14:05 +0300

    Code Optimization

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/eshop', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -35,6 +35,11 @@ app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
+
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
+
 // eslint-disable-next-line
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message});
