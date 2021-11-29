4945cb28d917e7c25cc9c27b71e9f34313f34c5a
NIR <MAILMAIL10@GMAIL.COM>
2021-11-30 00:50:39 +0200

    CodeOpt

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
 }).catch(() => console.log("mongo error"));
@@ -34,10 +34,12 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
-  });
+if (process.env.NODE_ENV === 'production') {
+    app.get(/^((?!(api)).)*$/, (req, res) => {
 
+        res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
